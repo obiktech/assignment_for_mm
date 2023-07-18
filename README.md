496f03c40086da0f1a17b93af3d517bbe9789978
obiktech <obiktechnology@gmail.com>
2023-07-18 10:37:56 +0530

    commit

--- README.md
+++ README.md
@@ -16,3 +16,11 @@ then finally run using python run.py
 
 
 debug mode can switch using .env file modification , not the code 
+
+
+employee can register using  site-address/employee/register
+by sending data in format like  {"email": email, "password": password} 
+
+
+and employee can login using  site-address/employee/login
+by sending data in format like  {"email": email, "password": password} and get a web token 
--- app/__init__.py
+++ app/__init__.py
@@ -6,14 +6,18 @@ from flask_sqlalchemy import SQLAlchemy
 from .utils import error_handlers
 import os
 from dotenv import load_dotenv
+from flask_cors import CORS
 load_dotenv(".env")
 
 app = Flask(__name__)
+CORS(app)
+
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = os.getenv(
     "SQLALCHEMY_TRACK_MODIFICATIONS")
 app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
